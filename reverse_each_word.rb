bd1a2abc5d8b342aad8b6ce43946e151f0ebdd42
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 03:16:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,6 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
   words.each {|word| word = word.reverse}
+  result = ""
+  words.each_with_index {|word, index| index != 0 ? result << " #{word}"}
 end
